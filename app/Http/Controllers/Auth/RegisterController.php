9394ed021e3d914d7cb9e02d240390fdfb50c50b
abraham <abrahamaguvasu@gmail.com>
2020-01-08 13:30:41 +0300

    changed welcome mail setup

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,12 +66,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        User::create([
+        Mail::to($data['email'])->send(new WelcomeEmail());
+
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
 
-        return Mail::to($data['email'])->send(new WelcomeEmail());
     }
 }
